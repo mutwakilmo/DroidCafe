1e5aca294851988be56f14cbc38519b035213f59
mutwakilmo <mutwakilmo@gmail.com>
2020-05-15 15:40:04 +0200

    order

--- app/src/main/java/com/mutwakilmo/droidcafe/OrderActivity.java
+++ app/src/main/java/com/mutwakilmo/droidcafe/OrderActivity.java
@@ -3,21 +3,125 @@ package com.mutwakilmo.droidcafe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class OrderActivity extends AppCompatActivity {
+import java.lang.reflect.Array;
+
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if (spinner != null){
+            spinner.setOnItemSelectedListener(this);
+        }
+        //Create ArrayAdapter
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears.
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Apply the adapter to the spinner
+        if (spinner != null){
+            spinner.setAdapter(adapter);
+        }
+
+        EditText editText = findViewById(R.id.phone_text);
+        if (editText != null){
+            editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                    boolean handled = false;
+                    if (actionId == EditorInfo.IME_ACTION_SEND){
+                        dialNumber();
+                        handled = true;
+                    }
+                    return handled;
+                }
+            });
+        }
+
+
+
+
+    }
+
+    private void dialNumber() {
+        //find the editText
+        EditText editText = findViewById(R.id.phone_text);
+        String phoneNum = null;
+        if (editText != null) phoneNum = "tel:" + editText.getText().toString();
+
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse(phoneNum));
+
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+    }
+
+    public void onRadioButtonClicked(View view) {
+        //IS the Button now checked
+        boolean checked = ((RadioButton) view).isChecked();
+        //check which radio button was clicked
+        switch (view.getId()){
+            case R.id.sameday:
+                if (checked)
+                    //same day service
+                    displayToast(getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.nextday:
+                if (checked)
+                    //Next day delivery
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case  R.id.pickup:
+                if (checked)
+                    //Pickup
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                //do nothing
+                break;
+        }
+    }
+
+    public void displayToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+
+    //spinner
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String spinnerLabel = parent.getItemAtPosition(position).toString();
+        displayToast(spinnerLabel);
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
